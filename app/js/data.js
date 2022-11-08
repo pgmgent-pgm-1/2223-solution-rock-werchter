28b60e918c3a6d32c47591ea68df46ea4c70e724
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2022-11-08 13:57:01 +0100

    Added so much code

---
+++ app/js/data.js
@@ -0,0 +1,211 @@
+const lineup = [
+  {
+    id: '80c73346-57e0-4bcc-97c6-a6873c35c973',
+    band: {
+      name: 'Stromae',
+      synopsis: 'Stromae bracht in 2010 zijn eerste album ‘Cheese’ uit. In 2013 volgde de wereldroem dankzij ‘Racine Carrée’. ​ En toen werd het stil. Voor de lancering van één van de collecties van modelabel mosaert bracht hij de single ‘Défiler’ uit. In 2019 speelde hij met Coldplay ‘Arabesque’ tijdens hun albumvoorstelling in Jordanië. Met zijn broer regisseerde Stromae clips voor Billie Eilish en Dua Lipa. En dan was daar vorig jaar de grote terugkeer, door de grootste poort met derde album “Multitude”. Stromae’s eigen interpretatie van wereldmuziek. Verbindend. Vertrouwd maar nog boeiender. In 2023 is hij één van de absolute headliners van Rock Werchter.',
+      socials: {
+        website: 'https://www.stromae.com/',
+        youtube: 'https://www.youtube.com/c/stromaeofficiel',
+        spotify: 'https://open.spotify.com/artist/5j4HeCoUlzhfWtjAfM1acR?si=e39bc47879c24d90',
+        facebook: 'https://www.facebook.com/stromae',
+        twitter: 'https://twitter.com/stromae',
+        instagram: 'https://www.instagram.com/stromae/',
+      },
+      picture: {
+        small: 'https://www.rockwerchter.be/media/cache/default_band/upload/media/default/c64d/a5/51296a48e400a72b91fe8b422451603a0e9aa439.jpg'
+      },
+      media: [
+        {
+          link: 'https://youtu.be/1LfgyPn8Byk',
+          type: 'youtube',
+        },
+        {
+          link: 'https://images0.persgroep.net/rcs/Nsiqubjk0ZYFuKPtHrpq_jaNgDU/diocontent/213093733/_fitwidth/1240?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.9',
+          type: 'image',
+        },
+      ]
+    },
+    place: {
+      name: 'Main Stage'
+    },
+    from: 1688069700000,
+    to: 1688075700000,
+    isHeadliner: true,
+    reviews: [
+      {
+        rating: 9,
+        comment: 'First of all, wow! The show immediately grabs you with its first couple of scenes, letting you know that this show is going to have a more adult tone. Seeing the workings of everyday people in the galaxy, the perspective of an imperial loyalist, and a thief making his way through the galaxy is so thrilling and exciting. The big and little details you get out of all the varying cultures on different planets allows you to completely melt into and engross yourself in the Star Wars galaxy, which is what many of us love so much.',
+        reviewedAt: 1663864320000
+      },
+      {
+        rating: 8,
+        comment: 'Andor has proven to be my favorite of the Star Wars Disney+ series, because it simply provides the basics of storytelling. Characters have motivation and personality, the dialogue is smart and snappy, and the stakes feel personal and real. The production team has also done a wonderful job creating a world that looks and sounds blue collar - no Jedi or Sith, just billions of working persons trying to make a living and simply get by. All the flash of Star Wars - the lightsaber fights, space battles and blaster fights, come second to simply telling a decent story. And for that alone, I think Andor is a step in the right direction.',
+        reviewedAt: 1660738920000
+      },
+      {
+        rating: 8,
+        comment: 'I Hope we could see more like this in the future... Disney have some problems with storylines from old characters but the news stories like mandalorian they nailed it... Please make this tone for the rest of the franchise. Star Wars should be a grown-up thing like most of the fans that grow up with. Stop wasting our time with current political agenda and give us timeless stories we can remember like the first movies from George Lucas. Thanks you Disney for let this happen.',
+        reviewedAt: 1658733060000
+      }
+    ],
+    comments: [
+      {
+        nickName: 'JaPPa03',
+        message: 'In feite zijn deze camera’s een soort update die je doorgaans na interne feedback zou doorvoeren voordat je een product op de markt brengt. Vooral de dual card slot is iets wat ze verkeerd hebben ingeschat terwijl het geen nieuwe kritiek is op camera’s.',
+        comments: [
+          {
+            nickName: 'Inoble',
+            message: 'Precies dat idee heb ik ook, een gevalletje D600, 800 > 610, 810. An sich natuurlijk een prima camera en feitelijk (aannemend) beter dan de voorgangers maar een upgrade niet waard wanneer je met Nikon schiet en graag bij Nikon wilt blijven.'
+          }
+        ]
+      },                
+    ]
+  },
+  {
+    id: 'c0274c53-ae17-4878-ae5a-2b02bfe19702',
+    band: {
+      name: 'Arctic Monkeys',
+      synopsis: 'Arctic Monkeys zijn Alex Turner, Jamie Cook, Nick O’Malley en Matt Helders. De band wordt in 2002 opgericht in het Engelse Sheffield. In 2006 schrijven ze muziekgeschiedenis met indrukwekkend debuut Whatever People Say I Am, That’s What I’m Not. Sindsdien zijn ze één van de meest succesvolle Britse bands. Dat vertaalt zich in duizelingwekkende streamingcijfers, een dikgevulde kast met awards en een digitale fanbase van meer dan 30 miljoen volgers. Zevende album The Car komt uit op vrijdag 21 oktober. Naar Werchter kwamen ze voor het eerst in 2006. Het publiek sloot hen in de armen om nooit meer los te laten. Na hun headline show op Rock Werchter 2018 schreef De Morgen: “Arctic Monkeys sloten Rock Werchter af met het perfecte festivalconcert voor 2018. Wars van trends. Wars van het gewicht van de rock-\'n-rollgeschiedenis. Met twee voeten vooruit en de blik op verdwaasd. Top notch.” Meer van dat op zondag 2 juli 2023!',
+      socials: {
+        website: 'https://arcticmonkeys.com/',
+        youtube: 'https://www.youtube.com/channel/UC-KTRBl9_6AX10-Y7IKwKdw',
+        spotify: 'https://open.spotify.com/artist/7Ln80lUS6He07XvHI8qqHH?si=16H4iXaBRs6WwD3v1KV3WA&nd=1',
+        facebook: 'https://arcticmonkeys.ffm.to/fb',
+        twitter: 'https://twitter.com/ArcticMonkeys',
+        instagram: 'https://www.instagram.com/arcticmonkeys/',
+      },
+      picture: {
+        small: 'https://www.rockwerchter.be/media/cache/default_band/upload/media/default/0d06/aa/daa228f07939589d2b5f34822b3a2f8c66d34d4a.jpg'
+      },
+      media: [
+        {
+          link: 'https://youtu.be/WMxEKlDtt_4',
+          type: 'youtube',
+        },
+        {
+          link: 'https://oor.nl/media/2022/08/130517-05-021_photo_credit_zackery_michael_hi_res-1024x683.jpg',
+          type: 'image',
+        },
+      ]
+    },
+    place: {
+      name: 'KluB C'
+    },
+    from: 1688323500000,
+    to: 1688327700000,
+    isHeadliner: false,
+    reviews: [
+      {
+        rating: 9,
+        comment: 'I finally watched The Mandalorian after putting it off for so long and I absolutely loved it! I don\'t consider myself to be a huge Star Wars superfan like some of these other fans out there but I am a regular fan that really enjoys watching the movies. I didn\'t even mind the latest trilogy, Rogue One and Solo more than most but The Mandalorian is without a doubt the best thing Star Wars related since the original trilogy.',
+        reviewedAt: 1657780980000
+      },
+      {
+        rating: 7,
+        comment: 'I\'ve been a fan of Star Wars since I was a child in the 1980s. As such, I was very disappointed with Disney\'s latest trilogy. Thankfully, shows like the Mandalorian manage to steer well clear of the atrociousness that those three films are.',
+        reviewedAt: 1656495060000
+      },
+      {
+        rating: 7,
+        comment: 'Another good spin-off series in an everlasting run of them it seems now. I\'ve always realized that the Star Wars franchise is puffed up and placed with "baby bumpers" to be extra kid friendly. But a prevailing criticism I have with the ever continuous series of comic book movies and series and the Star Wars franchise: If you\'re going to have such adult themes like battle, war and killing(sometimes with decapitation?!), show some blood every once in a while?? Any color blood. My god, I just don\'t understand why it isn\'t acceptable for these creators to show this dude killing multiples of people as a bounty hunter, then never show any of the blood that accompanies killing someone.',
+        reviewedAt: 1655999760000
+      }
+    ],
+    comments: [
+      {
+        nickName: 'Joris748',
+        message: 'Nice, goede iteratie van de Z6 en Z7. Veel van de kritiekpunten zijn aangepakt',
+        comments: [
+          {
+            nickName: 'JaPPa03',
+            message: 'Als de APS-C crop voor 4K/60 op de Z6 geen probleem voor je is, is het zeker een mooie upgrade.'
+          },
+        ]
+      },
+      {
+        nickName: 'Inoble',
+        message: 'Persoonlijk vind ik het toch een gemiste kans voor filmmakers. Nu zal die markt niet voornamelijk bij Nikon liggen maar ze zorgen er naar mijn idee toch voor dat ze zichzelf buiten spel zetten door de wensen te honoreren maar een cropfactor in te brengen',
+        comments: [
+          {
+            nickName: 'JaPPa03',
+            message: 'In feite zijn deze camera’s een soort update die je doorgaans na interne feedback zou doorvoeren voordat je een product op de markt brengt. Vooral de dual card slot is iets wat ze verkeerd hebben ingeschat terwijl het geen nieuwe kritiek is op camera’s.',
+          },                
+        ]
+      }
+    ],
+  },
+  {
+    id: '69d09eb0-37f1-4521-829e-2154db7e406f',
+    band: {
+      name: 'Muse',
+      synopsis: 'Matt Bellamy, Dominic Howard en Chris Wolstenholme vormen sinds 1994 het powertrio Muse. Negen studioalbums hebben ze tot op heden uitgebracht. Het aantal verkochte albums staat op 30 miljoen. Jongste album Will of the People debuteerde in meerdere landen, waaronder thuisland Verenigd Koninkrijk, Frankrijk en Italië op één. Ook bij ons positioneerde het album zich meteen comfortabel in de hoogste regionen – op #2 in Vlaanderen, op #1 in Wallonië. Muse is een absolute topper. In de prijzenkast: twee Grammy Awards, een American Music Award, vijf MTV Europe Music Awards, twee Brit Awards, elf NME Awards en zeven Q Awards. Muse en Rock Werchter gaan ‘way back’. De Britten waren er voor het eerst in 2000. Ze zijn uitgegroeid tot ware publieksfavorieten.',
+      socials: {
+        website: 'https://www.muse.mu/',
+        youtube: 'https://www.youtube.com/muse',
+        spotify: 'https://open.spotify.com/artist/12Chz98pHFMPJEknJQMWvI',
+        facebook: 'https://www.facebook.com/muse',
+        twitter: 'https://twitter.com/muse',
+        instagram: 'https://instagram.com/muse',
+      },
+      picture: {
+        small: 'https://www.rockwerchter.be/media/cache/default_band/upload/media/default/0fe6/b8/188d68de6d211732f1e496c5a826b9536a78d7af.jpg'
+      },
+      media: [
+        {
+          link: 'https://youtu.be/8muGWOjNoSo',
+          type: 'youtube',
+        },
+        {
+          link: 'https://oor.nl/media/2022/08/130517-05-021_photo_credit_zackery_michael_hi_res-1024x683.jpg',
+          type: 'image',
+        },
+      ]
+    },
+    place: {
+      name: 'Main Stage'
+    },
+    from: 1688244300000,
+    to: 1688249700000,
+    isHeadliner: true,
+    reviews: [
+      {
+        rating: 9,
+        comment: 'I finally watched The Mandalorian after putting it off for so long and I absolutely loved it! I don\'t consider myself to be a huge Star Wars superfan like some of these other fans out there but I am a regular fan that really enjoys watching the movies. I didn\'t even mind the latest trilogy, Rogue One and Solo more than most but The Mandalorian is without a doubt the best thing Star Wars related since the original trilogy.',
+        reviewedAt: 1657780980000
+      },
+      {
+        rating: 7,
+        comment: 'I\'ve been a fan of Star Wars since I was a child in the 1980s. As such, I was very disappointed with Disney\'s latest trilogy. Thankfully, shows like the Mandalorian manage to steer well clear of the atrociousness that those three films are.',
+        reviewedAt: 1656495060000
+      },
+      {
+        rating: 7,
+        comment: 'Another good spin-off series in an everlasting run of them it seems now. I\'ve always realized that the Star Wars franchise is puffed up and placed with "baby bumpers" to be extra kid friendly. But a prevailing criticism I have with the ever continuous series of comic book movies and series and the Star Wars franchise: If you\'re going to have such adult themes like battle, war and killing(sometimes with decapitation?!), show some blood every once in a while?? Any color blood. My god, I just don\'t understand why it isn\'t acceptable for these creators to show this dude killing multiples of people as a bounty hunter, then never show any of the blood that accompanies killing someone.',
+        reviewedAt: 1655999760000
+      }
+    ],
+    comments: [
+      {
+        nickName: 'Joris748',
+        message: 'Nice, goede iteratie van de Z6 en Z7. Veel van de kritiekpunten zijn aangepakt',
+        comments: [
+          {
+            nickName: 'JaPPa03',
+            message: 'Als de APS-C crop voor 4K/60 op de Z6 geen probleem voor je is, is het zeker een mooie upgrade.'
+          },
+        ]
+      },
+      {
+        nickName: 'Inoble',
+        message: 'Persoonlijk vind ik het toch een gemiste kans voor filmmakers. Nu zal die markt niet voornamelijk bij Nikon liggen maar ze zorgen er naar mijn idee toch voor dat ze zichzelf buiten spel zetten door de wensen te honoreren maar een cropfactor in te brengen',
+        comments: [
+          {
+            nickName: 'JaPPa03',
+            message: 'In feite zijn deze camera’s een soort update die je doorgaans na interne feedback zou doorvoeren voordat je een product op de markt brengt. Vooral de dual card slot is iets wat ze verkeerd hebben ingeschat terwijl het geen nieuwe kritiek is op camera’s.',
+          },                
+        ]
+      }
+    ],
+  },
+];
---
+++ app/js/main.js
@@ -0,0 +1,103 @@
+(() => {
+  const app = {
+    init() {
+      console.log('1. Initialize Application');
+      this.cacheElements();
+      this.generateUI();
+    },
+    cacheElements() {
+      console.log('2. Cache Elements');
+      this.$lineup = document.querySelector('.lineup');
+      this.$concertDetails = document.querySelector('.concert-details');
+    },
+    generateUI() {
+      console.log('3. Generate User Interface');
+      this.$lineup.innerHTML = this.generateHTMLForLineup(lineup);
+      const $concerts = this.$lineup.querySelectorAll('.concert');
+      $concerts.forEach(($concert) => {
+        $concert.addEventListener('click', (ev) => {
+          const id = ev.currentTarget.dataset.id;
+          this.generateUIForConcertDetails(id);
+        });
+      });
+    },
+    generateHTMLForLineup(concerts) {
+      let tmpStr = '';
+      for (let i = 0; i < concerts.length; i++) {
+        tmpStr += this.generateHTMLForConcert(concerts[i]);
+      }
+      return tmpStr;
+    },
+    generateHTMLForConcert(concert) {
+      return `
+      <div class="concert" data-id="${concert.id}">
+        <h2 class="concert__band">${concert.band.name}</h2>
+        <picture class="concert__picture">
+          <img src="${concert.band.picture.small}">
+        </picture>
+        <div class="concert__meta">
+          <span class="date__from">${new Date(concert.from).toDateString()}</span>
+        </div>
+      </div>
+      `;
+    },
+    generateUIForConcertDetails(id) {
+      const concert = lineup.find((concert) => concert.id === id);
+      this.$concertDetails.innerHTML = this.generateHTMLForConcertDetails(concert);
+    },
+    generateHTMLForConcertDetails(concert) {
+      return `
+      <div class="concert-details__left">
+        <picture class="concert-details__picture">
+          <img src="${concert.band.picture.small}">
+        </picture>
+      </div>
+      <div class="concert-details__right">
+        <h1>${concert.band.name}</h1>
+        <ul class="socials__list">
+          ${this.generateHTMLForSocials(concert.band.socials)}
+        </ul>
+        <p>${concert.band.synopsis}</p>
+        <div class="concert-details__media">
+          ${this.generateHTMLForMedia(concert.band.media)}
+        </div>
+      </div>
+      `;
+    },
+    generateHTMLForSocials(socials) {
+      let tmpStr = '';
+      for (let prop in socials) {
+        tmpStr += `<li><a href="${socials[prop]}" target="__blank" rel="noreferrer nofollow">${this.generateHTMLForSocialMediaIcon(prop)}</i><span>${prop}</span></a></li>`
+      }
+      return tmpStr;
+    },
+    generateHTMLForSocialMediaIcon(socialMedia) {
+      switch (socialMedia) {
+        case 'website': default: return '<i class="bi bi-globe">';
+        case 'youtube':return '<i class="bi bi-youtube">';
+        case 'spotify': return '<i class="bi bi-spotify">';
+        case 'facebook': return '<i class="bi bi-facebook">';
+        case 'instagram': return '<i class="bi bi-instagram">';
+        case 'twitter': return '<i class="bi bi-twitter">';
+      }
+    },
+    generateHTMLForMedia(media) {
+      return media.map(medium => {
+        let tmpStr = '';
+        switch (medium.type) {
+          case 'youtube': tmpStr = `
+          <iframe width="702" height="395" src="${medium.link}" title="MUSE - Blockades [Official Music Video]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          `; break;
+          case 'image': default: tmpStr = `
+          <picture class="concert-details__medium-picture">
+            <img src="${medium.link}">
+          </picture>
+          `; break;
+        }
+        return tmpStr;
+      }).join('');
+      return tmpStr;
+    },
+  };
+  app.init();
+})();
